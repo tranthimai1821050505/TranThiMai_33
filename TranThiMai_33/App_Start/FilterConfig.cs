e59bdfb6b9fa68c6830f6edf5c4744ed40794f92
MAI TRAN THI <1821050505@student.humg.edu.vn>
2021-06-06 15:03:54 +0700

    Add project files.

---
+++ TranThiMai_33/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TranThiMai_33
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TranThiMai_33/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TranThiMai_33.MvcApplication" Language="C#" %>
